94dabbc6e316486f945a9f723f504c92cd52225d
omer bental <ombental@gmail.com>
2024-05-15 10:21:34 +0300

    fix ci.yml

--- serafin/settings_tests.py
+++ serafin/settings_tests.py
@@ -7,16 +7,4 @@ TEST_DISCOVER_TOP_LEVEL = BASE_DIR
 TEST_DISCOVER_ROOT = BASE_DIR
 TEST_DISCOVER_PATTERN = 'test_*'
 
-# In-memory test database
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': ':memory:',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
-    },
-}
-
 HUEY.update({'always_eager': True})

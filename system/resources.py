4f22c3a478c8ea7804324222eaeccf18febde576
ombental <omer-bental@noal.org.il>
2024-08-04 14:24:51 +0300

    fix imports in resources.py

--- system/resources.py
+++ system/resources.py
@@ -1,32 +1,12 @@
-import logging
-
 from import_export.resources import ModelResource
 
-from system.application_services import ProgramAfterImportRowHandler, ProgramBeforeImportRowHandler
 from system.models import Program
 
-logger = logging.getLogger("debug")
-
+# This is so the classic behavior of huey import export can behave correctly
 
 class ProgramImportResource(ModelResource):
     class Meta:
         model = Program
-        fields = ['title', 'display_title', 'about', 'style', 'default_program_start_time',
-                  'admin_note', 'is_lock']
-        import_id_fields = ['title']
-
-    def import_row(self, row, instance_loader, **kwargs):
-        logger.debug(f"Starting import, row data\n{row}")
-        edited_row = ProgramBeforeImportRowHandler(row).run()
-        return super().import_row(edited_row, instance_loader, **kwargs)
-
-    def after_import_row(self, row, row_result, **kwargs):
-        program = row_result.instance if row_result.instance else row_result.original
-        if program is None:
-            program = Program.objects.get(id=row_result.object_id)
-        logger.debug(f"Handling program after import")
-        ProgramAfterImportRowHandler(program, row).import_program_data()
-        return super().after_import_row(row, row_result, **kwargs)
 
 class ProgramExportResource(ModelResource):
     class Meta:

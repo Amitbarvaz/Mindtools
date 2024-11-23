24040aec5459d45a94705700b8c0f66ff15abefd
Omer Bental <ombental@gmail.com>
2024-11-23 16:21:58 +0200

    fix edit_admin_ips.sh

--- edit_admin_ips.sh
+++ edit_admin_ips.sh
@@ -1,33 +1,110 @@
 #!/bin/bash
 
-# 1. Read environment variables from .env file
-source .env
+filename=".env"
 
-# 2. Print the value of ALLOWED_ADMIN_IPS
-echo "Current allowed IPs: $ALLOWED_ADMIN_IPS"
+if [ "$1" = "-h" ] || [ "$1" = "--help" ] || ([ "$1" != "add" ] && [ "$1" != "remove" ] && [ "$1" != "show" ])
+  then
+    echo "Usage: $0 COMMAND [OPTIONS] [IP]
+        Display or edit ip list for admin restrictions
+        Example: $0 add 192.168.0.1
+                         $0 show
+                         $0 remove -r 192.168.0.0/24
+                         $0 add -r 192.168.0.0
 
+        COMMAND:
+                The available commands are add, remove and show
 
-# 3. Prompt the user for editing
-read -p "Would you like to edit the IP list? (yes/no): " edit_ip_list
+        OPTIONS:
+                -r, --range             Specify it's IP range and not a single IP
+                -f, --filename          Specify path to env file
+                -n, --no-reset          Don't reset app service after editing
+                -t, --test              Don't change file or reset the service, just show the changes"
+    exit 1
+fi
 
-# 4. If the user wants to edit, prompt for new IP list
-if [[ $edit_ip_list == "yes" ]]; then
-  read -p "Enter the new comma-separated IP list: " new_ip_list
+ip="${!#}"
+new_ips=""
+setting_key="DJANGO_ALLOWED_ADMIN_IPS"
+is_test=0
 
-  if [[ $new_ip_list =~ ^[0-9.]+(,[0-9.]+)*$ ]]; then
-    echo "Editing and Restarting Server..."
+if [ "$1" = "add" ] || [ "$1" = "remove" ]; then
+        reset_app=1
+else
+        reset_app=0
+fi
 
-    # 5. Edit the .env file
-    sed -i "s/ALLOWED_ADMIN_IPS=.*/ALLOWED_ADMIN_IPS=$new_ip_list/" .env
+for (( i=2; i < "$#"; i++ )); do
 
-    source .env
+        if [ "${!i}" = "-r" ] || [ "${!i}" = "--range" ]; then
+                ip_without_last_seg="${ip%.*}"
+                ip="${ip_without_last_seg}.0/24"
+                setting_key="DJANGO_ALLOWED_ADMIN_IP_RANGES"
+        fi
+        if [ "${!i}" = "-f" ]; then
+              filename="${!i}"
+        fi
+        if [ "${!i}" = "-n" ] || [ "${!i}" = "--no-reset" ]; then
+                reset_app=0
+        fi
+        if [ "${!i}" = "-t" ] || [ "${!i}" = "--test" ]; then
+                is_test=1
+        fi
+done
 
-    # 6. Restart the Docker Compose service
-    docker compose -f DOCKER_COMPOSE_FILE_NAME! restart APP_SERVICE_NAME
+if [ ! -f "$filename" ]; then
+    echo "$filename does not exist. Exiting program"
+        exit 1
+fi
 
-    echo "Successfully updated allowed_admin_ips list and restarted the server."
+ips=$(grep "^$setting_key=" $filename | cut -d'=' -f2)
+IFS=',' read -r -a ip_array <<< $ips
 
-  else
-    echo "Invalid input. Exiting Script."
-  fi
+if [ "$1" = "add" ]; then
+        ip_array[${#ip_array[@]}]="$ip"
+        for value in "${ip_array[@]}"
+        do
+                new_ips="${new_ips}${value},"
+        done
+        new_ips="${new_ips%?}"
+fi
+
+if [ "$1" = "remove" ]; then
+        found=0
+        for value in "${ip_array[@]}"
+        do
+                if [ "$value" = "$ip" ]
+                then
+                        found=1
+                else
+                        new_ips="${new_ips}${value},"
+                fi
+        done
+        if [ $found -eq 0 ]
+        then
+                echo Could not find IP "$ip" to remove
+                exit 1
+        fi
+        new_ips="${new_ips%?}"
+fi
+
+if [ "$1" = "show" ]; then
+        echo "Current IPs: ${ip_array[@]}"
+fi
+
+new_ips="${new_ips}"
+
+if [ "$1" = "add" ] || [ "$1" = "remove" ]; then
+        if [ $is_test -eq 0 ]; then
+                sed -i "s/$setting_key=.*/$setting_key=$new_ips/" $filename
+                echo Changed Settings
+
+                if [ $reset_app -eq 1 ]; then
+                        sudo docker compose -f docker-compose-deploy-temp-volumes.yml down app
+                        sudo docker compose -f docker-compose-deploy-temp-volumes.yml up -d app
+                        echo Service Restarted
+                fi
+
+        else
+                echo "$ips" ">>" "$new_ips"
+        fi
 fi
